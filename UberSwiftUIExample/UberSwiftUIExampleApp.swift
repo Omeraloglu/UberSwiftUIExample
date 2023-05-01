17cb03338cfc2e6b55f60637b4d0974a0ea43981
Omer <omeraloglu16@hotmail.com>
2023-05-01 17:31:48 +0300

    Initial Commit

---
+++ UberSwiftUIExample/UberSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIExampleApp.swift
+//  UberSwiftUIExample
+//
+//  Created by Ömer ALOĞLU on 1.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
